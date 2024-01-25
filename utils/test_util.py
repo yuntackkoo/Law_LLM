8efebccdb032cbb7adb2bd21b17afb10c6e2fa4a
yuntack koo <flyiceball@gmail.com>
2024-01-25 08:54:42 +0000

    fix unit test utils

--- utils/test_util.py
+++ utils/test_util.py
@@ -1,26 +1,31 @@
-import utils
+import util
 
 
 def test_single_number_parse_asnwer():
-    result = utils.parse_answer("[4], <답변>")
+    question = "[4], <답변>"
+    result = util.parse_answer(question)
     assert result == ([4], "답변")
 
 
 def test_multi_number_parse_asnwer():
-    result = utils.parse_answer("[1, 4], <답변>")
+    question = "[1, 4], <답변>"
+    result = util.parse_answer(question)
     assert result == ([1, 4], "답변")
 
 
 def test_multi_number_no_blank_parse_answer():
-    result = utils.parse_answer("[1,4], <답변>")
+    question = "[1,4], <답변>"
+    result = util.parse_answer(question)
     assert result == ([1, 4], "답변")
 
 
 def test_invalid_answer_parse_answer():
-    result = utils.parse_answer("[1, 4], ")
-    assert result == None
+    question = "[1, 4], "
+    result = util.parse_answer(question)
+    assert result == ([0], question)
 
 
 def test_invalid_number_answer_parse_answer():
-    result = utils.parse_answer("[1, , <답변>")
-    assert result == None
+    question = "[1, , <답변>"
+    result = util.parse_answer(question)
+    assert result == ([0], question)

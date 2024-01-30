c62f2dd305589f7c3138333d00358e98e6261f9b
yuntackkoo <flyiceball@gmail.com>
2024-01-30 17:02:36 +0900

    Pass ci (#2)
    * rename unit test workflow name
    
    * fix unit test utils
    
    * add pylintrc file
    
    * fix pylint for main.py
    
    * fix pylint for data model
    
    * pylint install requirement
    
    * apply tensor flow pylint configuration
    
    * apply format

--- data_model/output_format.py
+++ data_model/output_format.py
@@ -2,41 +2,41 @@ import json
 
 
 class OutputFormatEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, OutputFormat):  # YourClass에는 실제 클래스 이름을 사용합니다.
-            return {
-                "question_number": obj.question_number,
-                "question": obj.question,
-                "gt_number": obj.gt_number,
-                "predict_number": obj.predict_number,
-                "predict_solution": obj.predict_solution,
-                "rag_confidence": obj.rag_confidence,
-                "rag_content": obj.rag_content,
-            }
-        return super(OutputFormatEncoder, self).default(obj)
+  def default(self, o):
+    if isinstance(o, OutputFormat):  # YourClass에는 실제 클래스 이름을 사용합니다.
+      return {
+          "question_number": o.question_number,
+          "question": o.question,
+          "gt_number": o.gt_number,
+          "predict_number": o.predict_number,
+          "predict_solution": o.predict_solution,
+          "rag_confidence": o.rag_confidence,
+          "rag_content": o.rag_content,
+      }
+    return super(OutputFormatEncoder, self).default(o)
 
 
 def convert_to_dict(obj):
-    if isinstance(obj, OutputFormat):
-        return obj.__dict__
-    return obj
+  if isinstance(obj, OutputFormat):
+    return obj.__dict__
+  return obj
 
 
 class OutputFormat:
-    def __init__(
-        self,
-        question_number: str,
-        question: str,
-        gt_number: list[int],
-        predict_number: list[int],
-        predict_solution: str,
-        rag_confidence: float,
-        rag_content: str,
-    ) -> None:
-        self.question_number = question_number
-        self.question = question
-        self.gt_number = gt_number
-        self.predict_number = predict_number
-        self.predict_solution = predict_solution
-        self.rag_confidence = rag_confidence
-        self.rag_content = rag_content
+  def __init__(
+      self,
+      question_number: str,
+      question: str,
+      gt_number: list[int],
+      predict_number: list[int],
+      predict_solution: str,
+      rag_confidence: float,
+      rag_content: str,
+  ) -> None:
+    self.question_number = question_number
+    self.question = question
+    self.gt_number = gt_number
+    self.predict_number = predict_number
+    self.predict_solution = predict_solution
+    self.rag_confidence = rag_confidence
+    self.rag_content = rag_content
--- utils/util.py
+++ utils/util.py
@@ -3,16 +3,16 @@ import ast
 
 
 def parse_answer(answer: str) -> tuple:
-    numbers = [0]
-    explain = answer
-    try:
-        pattern = re.compile(r"\[([\d,\s]+)\],\s*<([^>]+)>")
-        mat = pattern.match(answer)
+  numbers = [0]
+  explain = answer
+  try:
+    pattern = re.compile(r"\[([\d,\s]+)\],\s*<([^>]+)>")
+    mat = pattern.match(answer)
 
-        if mat:
-            numbers = list(map(ast.literal_eval, mat.group(1).split(",")))
-            explain = mat.group(2)
-    except (ValueError, SyntaxError) as e:
-        print(f"Error: {e}")
-    finally:
-        return numbers, explain
+    if mat:
+      numbers = list(map(ast.literal_eval, mat.group(1).split(",")))
+      explain = mat.group(2)
+  except (ValueError, SyntaxError) as e:
+    print(f"Error: {e}")
+  finally:
+    return numbers, explain
